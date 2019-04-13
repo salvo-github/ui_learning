7b45cc274fd14a7f3e64b17f0f5de373a91fd0f6
Salvo <cappuccinosalvatore@gmail.com>
2019-04-13 13:51:02 +0200

    - from .sass to .scss - new component (login, logo, footer, header, pagecontent) - first attempt to style the components - todo: learn sass syntax, learn flexbox, learn about svg, learn about repeated background images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { PagecontentComponent } from './pagecontent/pagecontent.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    PagecontentComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
