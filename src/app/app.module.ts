504303f9333ceef76b4e0c11d68ca4ca61b43443
Salvo <cappuccinosalvatore@gmail.com>
2019-04-14 20:24:54 +0200

    - Added router logic - css is not the final one - moved components under "components" folder - todo: css positioning, css grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component';
-import { PagecontentComponent } from './pagecontent/pagecontent.component';
-import { LogoComponent } from './logo/logo.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,6 @@ import { LogoComponent } from './logo/logo.component';
     HeaderComponent,
     FooterComponent,
     LoginComponent,
-    PagecontentComponent,
     LogoComponent
   ],
   imports: [
