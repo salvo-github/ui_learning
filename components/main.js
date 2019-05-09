f8785248dace6e5e748a4be14da893d326c9b0cb
Salvo <cappuccinosalvatore@gmail.com>
2019-05-09 19:07:41 +0200

    for revision

---
+++ components/main.js
@@ -0,0 +1,8 @@
+function toogleFilters() {
+  var x = document.getElementById("filters__content-id");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
