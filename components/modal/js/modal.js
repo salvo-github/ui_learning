19ad3cb490df63be0e7d174191f5e7813a39dfeb
Salvo <cappuccinosalvatore@gmail.com>
2019-05-12 22:34:42 +0200

    -- Added icon for textinput fields -- Fixed login modal (login.html clink on login) -- Fixed filters in main (range it's missing)

--- components/modal/js/modal.js
+++ components/modal/js/modal.js
@@ -1,9 +1,9 @@
-function modalToggle(id) {
+function toggleDisplay(id) {
   const modal = document.querySelector(`#${id}`);
   const modalOverlay = document.querySelector("#modal-overlay");
 
-  modal.classList.toggle("modal--close");
-  modalOverlay.classList.toggle("modal--close");
+  modal.classList.toggle("element--close");
+  modalOverlay.classList.toggle("element--close");
 
   return false;
 }
