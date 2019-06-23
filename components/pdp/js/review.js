b9d07ed9737f51abb40a9b05bf17b377dac8ce99
Salvo <cappuccinosalvatore@gmail.com>
2019-06-23 19:17:19 +0200

    Added default review-preview css

---
+++ components/pdp/js/review.js
@@ -0,0 +1,11 @@
+const reviewTemplate = document.querySelector('#review__template');
+
+document.querySelector('.review__placeholder').append(reviewTemplate.content.cloneNode(true));
+
+
+
+// var content = ((document.querySelector('link[rel="import"]')) && document.querySelector('link[rel="import"]').import) || document;
+
+// shadow.append(content.getElementById('simple-slider__template').content.cloneNode(true));
+
+// this.imgElement = shadow.querySelector('.simple-slider__image');
