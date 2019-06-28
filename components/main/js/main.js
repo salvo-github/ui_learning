493df746fb3c06c5202f9cabd5f6cdf07af0fc1b
Salvo <cappuccinosalvatore@gmail.com>
2019-06-28 19:27:36 +0200

    course init

--- components/main/js/main.js
+++
@@ -1,8 +0,0 @@
-function toogleFilters() {
-  const x = document.getElementById("filters__content-id");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
--- components/modal/js/modal.js
+++
@@ -1,9 +0,0 @@
-function toggleDisplay(id) {
-  const modal = document.querySelector(`#${id}`);
-  const modalOverlay = document.querySelector("#modal-overlay");
-
-  modal.classList.toggle("element--close");
-  modalOverlay.classList.toggle("element--close");
-
-  return false;
-}
